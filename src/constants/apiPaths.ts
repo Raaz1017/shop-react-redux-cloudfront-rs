ad6fbdc4cbd857a408daf7f244702500783b22ac
Raaz1017 <azar.rd@mail.ru>
2022-07-24 19:42:04 +0300

    PR for Task 7 at RS for FE.

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://pdfohvxr26.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://6zsxufhbo9.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://2ufqcvjll8.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://pdfohvxr26.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://6zsxufhbo9.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
